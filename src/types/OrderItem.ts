adfe2646940797bac4e2f428e2814d249ffe729d
Mmy26 <yu.b1994masamura@gmail.com>
2022-03-10 16:35:51 +0900

    OrderItemクラスconstructor,getter,setterの作成

---
+++ src/types/OrderItem.ts
@@ -0,0 +1,70 @@
+import { Item } from "./Item";
+import { OrderTopping } from "./OrderTopping";
+
+export class OrderItem {
+  constructor(
+    private _id: number,
+    private _itemId: number,
+    private _orderId: number,
+    private _quantity: number,
+    private _size: number,
+    private _item: Item,
+    private _orderToppingList: Array<OrderTopping>
+  ) {}
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public get itemId(): number {
+    return this._itemId;
+  }
+
+  public set itemId(itemId: number) {
+    this._itemId = itemId;
+  }
+
+  public get orderId(): number {
+    return this._orderId;
+  }
+
+  public set orderId(orderId: number) {
+    this._orderId = orderId;
+  }
+
+  public get quantity(): number {
+    return this._quantity;
+  }
+
+  public set quantity(quantity: number) {
+    this._quantity = quantity;
+  }
+
+  public get size(): number {
+    return this._size;
+  }
+
+  public set size(size: number) {
+    this._size = size;
+  }
+
+  public get item(): Item {
+    return this._item;
+  }
+
+  public set item(item: Item) {
+    this._item = item;
+  }
+
+  public get orderToppingList(): Array<OrderTopping> {
+    return this._orderToppingList;
+  }
+
+  public set orderToppingList(orderToppingList: Array<OrderTopping>) {
+    this._orderToppingList = orderToppingList;
+  }
+}
